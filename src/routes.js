57f81029a2a45aaa9abed5864bae9f3bfae8ba3a
Françuel Soares <fs.francuelsoares@gmail.com>
2019-10-30 18:02:06 -0300

    0.8.0
    # New - Added User Control and Store Route

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Françuel',
-    email: 'fran@teste.com',
-    password_hash: 'dwvdyqfdyvhvbjhv'
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
